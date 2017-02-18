08c9dea980c5351ebfd943c77e7ce21d881eaedc
Gabor Nagy <gabor.nagy@stylersonline.com>
2017-02-18 09:26:23 +0100

    log xml váz

---
+++ src/java/model/XmlLog.java
@@ -0,0 +1,28 @@
+
+package model;
+
+import org.w3c.dom.Document;
+
+public class XmlLog {
+  
+  private static Document d=null;
+		
+  private static void loadXml() {
+    // csak akkor be beolvasni ha még nincs
+    if (d==null) {
+      // TODO olvassuk be az xml fájlt
+    }
+  }
+  
+  public static void log(String[] data) {
+    loadXml();
+    
+    
+  }
+  
+  public static ArrayList<String> getXmlContent() {
+    
+  }
+  
+  
+}
