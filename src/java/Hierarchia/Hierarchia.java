6f5a1b99278a58898525eb7fc581af8b33018434
Pomeisl Ferenc <ferencpomeisl@gmail.com>
2017-02-18 09:28:35 +0100

    h

--- src/java/Hierarchia/Hierarchia.java
+++ src/java/Hierarchia/Hierarchia.java
@@ -7,12 +7,19 @@ package Hierarchia;
 
 import java.awt.HeadlessException;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
 
 public class Hierarchia extends JFrame {
 
+  private JTree tree=new JTree();
+  
   public Hierarchia() throws HeadlessException {
     this.setTitle("Employee hierarchy");
+    this.setSize(400, 300);
+    this.setLocationRelativeTo(this);
     
+    this.add(new JScrollPane(tree));
   }
   
   public static void main(String[] args) {
