f88a34735811e25eee719ba6fba447befab3358a
Pomeisl Ferenc <ferencpomeisl@gmail.com>
2017-02-18 10:51:15 +0100

    probalkozasok

--- src/java/Hierarchia/Hierarchia.java
+++ src/java/Hierarchia/Hierarchia.java
@@ -2,9 +2,15 @@
 package Hierarchia;
 
 import java.awt.HeadlessException;
+import java.awt.event.HierarchyBoundsAdapter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
+import javax.swing.tree.DefaultTreeModel;
+import model.HierarchyModel;
 
 public class Hierarchia extends JFrame {
 
@@ -15,6 +21,13 @@ public class Hierarchia extends JFrame {
     this.setSize(400, 300);
     this.setLocationRelativeTo(this);
     
+    DefaultTreeModel treeModel=null;
+    try {
+      treeModel=new DefaultTreeModel(HierarchyModel.getModel());
+    } catch (SQLException|ClassNotFoundException ex) {
+      System.out.println("Hiba");
+    }
+    tree.setModel(treeModel);
     this.add(new JScrollPane(tree));
   }
   
